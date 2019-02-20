45a17f896db71ef089542fe3acf77d9cc125514d
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-20 10:52:37 -0500

    PostGres Migration

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,9 @@
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/dev.sqlite3"
-    },
+    client: "pg",
+    connection:
+      "postgres://bbipyydlkkifde:42f4430f3aa70862585d1fd891aa2316565f8acc1d017db7e9fdadb7f9fa9e57@ec2-54-83-196-179.compute-1.amazonaws.com:5432/d3nrvg3l93b21f",
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
